4e4d8ff949dd4b57b83a4340519b6458d89923d1
jabramov1 <90768325+jabramov1@users.noreply.github.com>
2025-10-28 21:00:23 -0700

    Update chat_with_pdf.py

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -71,7 +71,7 @@ if uploaded_files:
                 chunks = text_splitter.split_documents(documents)
                 all_chunks.extend(chunks)
                 
-                #close the file. Chatgpt recommneded using with
+                #DELETE the file
                 os.remove(tmp_file_path)
             
             # Create vector store from ALL chunks
